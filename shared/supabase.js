005de1c1fd176082fa1ad0b336b14cec804512ad
mnaukohutka <zpravy.robotu@outlook.cz>
2025-08-21 09:58:28 +0200

    Update supabase.js

--- shared/supabase.js
+++ shared/supabase.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Inicializace klienta s tvým projektem
 export const supabase = createClient(
-  'https://TVUJ-PROJEKT.supabase.co',    // nahraď adresou tvého Supabase projektu
-  'TVUJ-ANON-KEY'                         // nahraď svým anonymním public klíčem
+  'https://dnidjzrljwlpifyhftwt.supabase.co',    // nahraď adresou tvého Supabase projektu
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuaWRqenJsandscGlmeWhmdHd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI3NzczNDMsImV4cCI6MjA2ODM1MzM0M30.L2-Jl5Ob0cMDAr-n0_KcOdaEr-87kWCb7c0QcXy3_Hw'                         // nahraď svým anonymním public klíčem
 );
 
